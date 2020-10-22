a846db7b3b19e4353af8e8109ec9a0d671ea8589
leandrocrs <contato@leandrw.com.br>
2020-10-21 23:02:02 -0300

    docs(ci): outputs for different formats

--- bili.config.ts
+++ bili.config.ts
@@ -3,7 +3,29 @@ import { Config } from 'bili';
 
 const config: Config = {
   input: 'src/index.ts',
-  output: { dir: 'lib' },
+  output: {
+    dir: 'lib',
+    moduleName: 'ReactWinston',
+    format: [
+      'amd',
+      'amd-min',
+      'cjs',
+      'cjs-min',
+      'commonjs',
+      'es',
+      'es-min',
+      'esm',
+      'esm-min',
+      'iife',
+      'iife-min',
+      'module',
+      'system',
+      'system-min',
+      'systemjs',
+      'umd',
+      'umd-min',
+    ],
+  },
 };
 
 export default config;
